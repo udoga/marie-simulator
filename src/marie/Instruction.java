71c511f242b819fbddf70775027efb68c3ac734d
Doga Sezgin <doga.sezgin@gmail.com>
2014-08-09 22:37:19 +0300

    instruction analyzes tokens

--- src/marie/Instruction.java
+++ src/marie/Instruction.java
@@ -1,38 +1,97 @@
 package marie;
 
+import java.util.Arrays;
+
 public class Instruction {
+
+    private static final String mriSymbolRegex = "(jns)|(load)|(store)|(add)|(subt)|(jump)|(addi)|(jumpi)|(org)";
+    private static final String nonMriSymbolRegex = "(input)|(output)|(halt)|(skipcond)|(clear)|(end)";
+    private static final String dataSymbolRegex = "(hex)|(dec)";
+
+    private static final String symbolRegex = mriSymbolRegex + "|" + nonMriSymbolRegex + "|" + dataSymbolRegex;
+    private static final String addressRegex = "([0-9][0-9a-fA-F]{0,2})|(0+[0-9a-fA-F]{0,3})";
+    private static final String addressLabelRegex = "[a-zA-Z]\\w*";
+    private static final String labelRegex = addressLabelRegex + ":";
+    private static final String hexDataRegex = "[a-fA-F0-9]+";
+    private static final String decDataRegex = "\\d+";
 
     private String symbol;
     private String address;
-
     private String label;
     private String addressLabel;
+    private String data;
 
-    public Instruction(String symbol) {
-        this.symbol = symbol;
+
+    public Instruction(String... instructionTokens) {
+        analyze(instructionTokens);
     }
 
-    public Instruction(String symbol, String address) {
-        this.symbol = symbol;
-        this.address = address;
+    private void analyze(String[] instructionTokens) {
+        if (instructionTokens.length == 0)
+            throw new InvalidInstruction();
+        instructionTokens = extractLabel(instructionTokens);
+        findAndSetSymbol(instructionTokens);
+        validateTokenCountBySymbolType(instructionTokens);
+        findAndSetAddress(instructionTokens);
+        findAndSetData(instructionTokens);
     }
 
-    public void setLabel(String label) {
-        this.label = label;
-    }
+        private String[] extractLabel(String[] instructionTokens) {
+            if (instructionTokens[0].matches(labelRegex)) {
+                label = instructionTokens[0].substring(0, instructionTokens[0].length()-1);
+                return Arrays.copyOfRange(instructionTokens, 1, instructionTokens.length);
+            } else return instructionTokens;
+        }
 
-    public void setAddressLabel(String addressLabel) {
-        this.addressLabel = addressLabel;
-    }
+        private void findAndSetSymbol(String[] instructionTokens) {
+            if (instructionTokens.length == 0)
+                throw new InvalidInstruction();
+            if (!instructionTokens[0].toLowerCase().matches(symbolRegex))
+                throw new InvalidInstruction();
+            symbol = instructionTokens[0].toLowerCase();
+        }
+
+        private void validateTokenCountBySymbolType(String[] instructionTokens) {
+            if (symbol.matches(mriSymbolRegex + "|" + dataSymbolRegex) && instructionTokens.length != 2)
+                throw new InvalidInstruction();
+            if (symbol.matches(nonMriSymbolRegex) && instructionTokens.length != 1)
+                throw new InvalidInstruction();
+        }
+
+        private void findAndSetAddress(String[] instructionTokens) {
+            if (symbol.matches(mriSymbolRegex)) {
+                if (instructionTokens[1].matches(addressRegex))
+                    address = instructionTokens[1];
+                else if (instructionTokens[1].matches(addressLabelRegex))
+                    addressLabel = instructionTokens[1];
+                else throw new InvalidInstruction();
+            }
+        }
+
+        private void findAndSetData(String[] instructionTokens) {
+            if (symbol.matches(dataSymbolRegex)) {
+                boolean hexDataValid = symbol.equals("hex") && instructionTokens[1].matches(hexDataRegex) &&
+                        (Integer.parseInt(instructionTokens[1], 16) <= 0xFFFF);
+                boolean decDataValid = symbol.equals("dec") && instructionTokens[1].matches(decDataRegex) &&
+                        (Integer.parseInt(instructionTokens[1]) <= 0xFFFF);
+                if (hexDataValid || decDataValid)
+                    data = instructionTokens[1];
+                else throw new InvalidInstruction();
+            }
+        }
 
     public String toString() {
         String labelInfo = (label == null)? "" : (", Label: " + label);
         String symbolInfo = (symbol == null)? "" : (", Symbol: " + symbol);
         String addressInfo = (address == null)? "" : (", Address: " + address);
         String addressLabelInfo = (addressLabel == null)? "" : (", Address Label: " + addressLabel);
+        String dataInfo = (data == null)? "" : (", Data: " + data);
 
-        String info = (labelInfo + symbolInfo + addressInfo + addressLabelInfo).substring(2);
+        String info = (labelInfo + symbolInfo + addressInfo + addressLabelInfo + dataInfo).substring(2);
         return "Instruction(" + info + ")";
+    }
+
+    public class InvalidInstruction extends RuntimeException {
     }
 
 }
--- test/marie/InstructionTest.java
+++ test/marie/InstructionTest.java
@@ -11,15 +11,56 @@ public class InstructionTest {
     @Test
     public void testInitialize() throws Exception {
         instruction = new Instruction("Halt");
-        assertEquals("Instruction(Symbol: Halt)", instruction.toString());
+        assertEquals("Instruction(Symbol: halt)", instruction.toString());
+        instruction = new Instruction("X:", "Halt");
+        assertEquals("Instruction(Label: X, Symbol: halt)", instruction.toString());
         instruction = new Instruction("Load", "100");
-        assertEquals("Instruction(Symbol: Load, Address: 100)", instruction.toString());
-        instruction = new Instruction("Dec", "20");
-        instruction.setLabel("X");
-        assertEquals("Instruction(Label: X, Symbol: Dec, Address: 20)", instruction.toString());
+        assertEquals("Instruction(Symbol: load, Address: 100)", instruction.toString());
+        instruction = new Instruction("X:", "Load", "100");
+        assertEquals("Instruction(Label: X, Symbol: load, Address: 100)", instruction.toString());
+        instruction = new Instruction("Load", "X");
+        assertEquals("Instruction(Symbol: load, Address Label: X)", instruction.toString());
+        instruction = new Instruction("X:", "Load", "Y");
+        assertEquals("Instruction(Label: X, Symbol: load, Address Label: Y)", instruction.toString());
+        instruction = new Instruction("Dec", "10");
+        assertEquals("Instruction(Symbol: dec, Data: 10)", instruction.toString());
+        instruction = new Instruction("X:", "Dec", "10");
+        assertEquals("Instruction(Label: X, Symbol: dec, Data: 10)", instruction.toString());
+    }
+
+    @Test(expected = Instruction.InvalidInstruction.class)
+    public void testThrowsInvalidInstruction_WhenMissingTokens() throws Exception {
+        instruction = new Instruction();
+    }
+
+    @Test(expected = Instruction.InvalidInstruction.class)
+    public void testThrowsInvalidInstruction_WhenWrongSymbol() throws Exception {
+        instruction = new Instruction("WrongSymbol", "10");
+    }
+
+    @Test(expected = Instruction.InvalidInstruction.class)
+    public void testThrowsInvalidInstruction_WhenOnlyLabel() throws Exception {
+        instruction = new Instruction("X:");
+    }
+
+    @Test(expected = Instruction.InvalidInstruction.class)
+    public void testThrowsInvalidInstruction_WhenOnlyMriSymbol() throws Exception {
         instruction = new Instruction("Load");
-        instruction.setAddressLabel("X");
-        assertEquals("Instruction(Symbol: Load, Address Label: X)", instruction.toString());
+    }
+
+    @Test(expected = Instruction.InvalidInstruction.class)
+    public void testThrowsInvalidInstruction_WhenAddressUsedInNonMri() throws Exception {
+        instruction = new Instruction("Halt", "100");
+    }
+
+    @Test(expected = Instruction.InvalidInstruction.class)
+    public void testThrowsInvalidInstruction_WhenDataIsInvalid() throws Exception {
+        instruction = new Instruction("Dec", "X");
+    }
+
+    @Test(expected = Instruction.InvalidInstruction.class)
+    public void testThrowsInvalidInstruction_WhenDataIsMoreThanMax() throws Exception {
+        instruction = new Instruction("Hex", "10000");
     }
 
 }
