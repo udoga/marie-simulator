604c022fb9a660957ef944512d4eac575a986947
Doga Sezgin <doga.sezgin@gmail.com>
2014-08-15 01:47:46 +0300

    add microprocessor

---
+++ src/marie/Microprocessor.java
@@ -0,0 +1,134 @@
+package marie;
+
+public class Microprocessor {
+
+    private Memory memory;
+
+    private int MAR;
+    private int PC;
+    private int MBR;
+    private int AC;
+    private int IR;
+    private int InREG;
+    private int OutREG;
+
+    private int opcode;
+    private boolean stopped;
+
+    public Microprocessor(Memory memory) {
+        this.memory = memory;
+    }
+
+    public void setProgramCounter(int value) {
+        PC = value;
+    }
+
+    public void run() {
+        stopped = false;
+        while (!stopped)
+            runNextInstruction();
+    }
+
+    public void runNextInstruction() {
+        fetch();
+        decode();
+        execute();
+    }
+
+    private void fetch() {
+        MAR = PC;
+        IR = memory.read(MAR);
+        PC = PC + 1;
+    }
+
+    private void decode() {
+        MAR = Integer.parseInt(String.format("%04x", IR).substring(1), 16);
+        opcode = Integer.parseInt(String.format("%04x", IR).substring(0, 1), 16);
+        MBR = memory.read(MAR);
+    }
+
+    private void execute() {
+        switch (opcode) {
+            case 0x0: jns(); break;
+            case 0x1: load(); break;
+            case 0x2: store(); break;
+            case 0x3: add(); break;
+            case 0x4: subt(); break;
+            case 0x5: input(); break;
+            case 0x6: output(); break;
+            case 0x7: halt(); break;
+            case 0x8: skipcond(); break;
+            case 0x9: jump(); break;
+            case 0xA: clear(); break;
+            case 0xB: addi(); break;
+            case 0xC: jumpi(); break;
+        }
+    }
+
+    private void jns() {
+        MBR = PC;
+        memory.write(MAR, MBR);
+        MBR = MAR;
+        AC = 1;
+        AC = AC + MBR;
+        PC = AC;
+    }
+
+    private void load() {
+        AC = MBR;
+    }
+
+    private void store() {
+        MBR = AC;
+        memory.write(MAR, MBR);
+    }
+
+    private void add() {
+        AC = AC + MBR;
+    }
+
+    private void subt() {
+        AC = AC - MBR;
+    }
+
+    private void input() {
+        AC = InREG;
+    }
+
+    private void output() {
+        OutREG = AC;
+    }
+
+    private void halt() {
+        stopped = true;
+    }
+
+    private void skipcond() {
+        String IRpart = String.format("%16s", Integer.toBinaryString(MAR)).replace(' ', '0').substring(4, 6);
+        if ((IRpart.equals("00") && AC < 0) || (IRpart.equals("01") && AC == 0) || (IRpart.equals("10") && AC > 0))
+            PC = PC + 1;
+    }
+
+    private void jump() {
+        PC = MAR;
+    }
+
+    private void clear() {
+        AC = 0;
+    }
+
+    private void addi() {
+        MAR = MBR;
+        MBR = memory.read(MAR);
+        AC = AC + MBR;
+    }
+
+    private void jumpi() {
+        PC = MBR;
+    }
+
+    public int[] getRegisterValues() {
+        return new int[]{AC, MAR, MBR, IR, PC, OutREG, InREG};
+    }
+
+}
---
+++ test/marie/MicroprocessorTest.java
@@ -0,0 +1,73 @@
+package marie;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MicroprocessorTest {
+
+    Memory memory = new Memory(0x1000);
+    Microprocessor microprocessor = new Microprocessor(memory);
+
+    @Test
+    public void testRunsBasicInstructions() throws Exception {
+        uploadProgramOne(memory);
+        microprocessor.setProgramCounter(0x100);
+                                    // AC      MAR     MBR     IR      PC      OutREG  InREG
+        runNextAndExpectRegisterValues(0x000A, 0x0106, 0x000A, 0x1106, 0x0101, 0x0000, 0x0000);
+        runNextAndExpectRegisterValues(0x001E, 0x0107, 0x0014, 0x3107, 0x0102, 0x0000, 0x0000);
+        runNextAndExpectRegisterValues(0x0019, 0x0108, 0x0005, 0x4108, 0x0103, 0x0000, 0x0000);
+        runNextAndExpectRegisterValues(0x0019, 0x0109, 0x0019, 0x2109, 0x0104, 0x0000, 0x0000);
+        assertEquals(0x0019, memory.read(0x109));
+        runNextAndExpectRegisterValues(0x0019, 0x0000, 0x0000, 0x6000, 0x0105, 0x0019, 0x0000);
+        runNextAndExpectRegisterValues(0x0019, 0x0000, 0x0000, 0x7000, 0x0106, 0x0019, 0x0000);
+    }
+
+    @Test
+    public void testRunProgram() throws Exception {
+        uploadProgramOne(memory);
+        microprocessor.setProgramCounter(0x100);
+        microprocessor.run();
+        assertEquals(0x0019, memory.read(0x109));
+    }
+
+    @Test
+    public void testRunsJumpInstructions() throws Exception {
+        uploadProgramTwo(memory);
+        microprocessor.setProgramCounter(0x100);
+                                    // AC      MAR     MBR     IR      PC      OutREG  InREG
+        runNextAndExpectRegisterValues(0x0000, 0x0000, 0x0000, 0x5000, 0x0101, 0x0000, 0x0000);
+        runNextAndExpectRegisterValues(0x0000, 0x0400, 0x0000, 0x8400, 0x0103, 0x0000, 0x0000);
+        runNextAndExpectRegisterValues(0x0109, 0x0108, 0x0108, 0x0108, 0x0109, 0x0000, 0x0000);
+        assertEquals(0x0104, memory.read(0x108));
+        runNextAndExpectRegisterValues(0x010A, 0x010C, 0x0001, 0xB10B, 0x010A, 0x0000, 0x0000);
+        runNextAndExpectRegisterValues(0x010A, 0x0108, 0x0104, 0xC108, 0x0104, 0x0000, 0x0000);
+        runNextAndExpectRegisterValues(0x0000, 0x0000, 0x0000, 0xA000, 0x0105, 0x0000, 0x0000);
+        runNextAndExpectRegisterValues(0x0000, 0x0107, 0x7000, 0x9107, 0x0107, 0x0000, 0x0000);
+        runNextAndExpectRegisterValues(0x0000, 0x0000, 0x0000, 0x7000, 0x0108, 0x0000, 0x0000);
+    }
+
+    private void runNextAndExpectRegisterValues(int... expectedRegisterValues) {
+        microprocessor.runNextInstruction();
+        assertArrayEquals(expectedRegisterValues, microprocessor.getRegisterValues());
+    }
+
+    private void uploadProgramOne(Memory memory) {
+    /*  ORG 100 / Load X / Add Y / Subt Z / Store RESULT / Output /
+        Halt / X: Dec 10 / Y: Dec 20 / Z: Dec 5 / RESULT: Dec 0 / END */
+        int originAddress = 0x100;
+        int[] hexCodes = {0x1106, 0x3107, 0x4108, 0x2109, 0x6000, 0x7000, 0x000A, 0x0014, 0x0005, 0x0000};
+        for (int i = 0; i < hexCodes.length; i++)
+            memory.write(originAddress+i, hexCodes[i]);
+    }
+
+    private void uploadProgramTwo(Memory memory) {
+    /*  ORG 100 / Input / Skipcond 400 / Load X / Jns Inc / Clear / Jump H
+        Load X / H: Halt / Inc: Dec 0 / AddI X / JumpI Inc / X: Hex 10C / Dec 1 */
+        int originAddress = 0x100;
+        int[] hexCodes = {0x5000, 0x8400, 0x110B, 0x0108, 0xA000, 0x9107,
+                0x110B, 0x7000, 0x0000, 0xB10B, 0xC108, 0x010C, 0x0001};
+        for (int i = 0; i < hexCodes.length; i++)
+            memory.write(originAddress+i, hexCodes[i]);
+    }
+
+}
